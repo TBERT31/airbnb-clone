285ca673868b464645fa47538840359afae9e604
TBERT31 <berteau.thomas@hotmail.fr>
2025-07-25 09:05:38 +0200

    feature: update can activate guard jwt-auth.guard

--- libs/common/src/auth/jwt-auth.guard.ts
+++ libs/common/src/auth/jwt-auth.guard.ts
@@ -1,21 +1,48 @@
-import { CanActivate, ExecutionContext, Inject, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { AUTH_SERVICE } from '../constants/services';
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  Logger,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { Reflector } from '@nestjs/core';
+import { catchError, map, Observable, of, tap } from 'rxjs';
+import { AUTH_SERVICE } from '../constants/services';
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
+    private readonly logger = new Logger(JwtAuthGuard.name);
+
     constructor(
         @Inject(AUTH_SERVICE) private readonly authClient: ClientProxy,
-    ){}
+        private readonly reflector: Reflector,
+    ) {}
     
     canActivate(
         context: ExecutionContext,
     ): boolean | Promise<boolean> | Observable<boolean> {
-        const jwt = context.switchToHttp().getRequest().cookies?.Authentication;
-        if(!jwt) {
-            return false;
+        const jwt =
+            context.switchToHttp().getRequest().cookies?.Authentication ||
+            context.switchToHttp().getRequest().headers?.authentication;
+
+        if (!jwt) {
+        return false;
         }
-        return true;
+
+        return this.authClient
+            .send('authenticate', {
+                Authentication: jwt
+            }).pipe(
+                tap((res) => {
+                    context.switchToHttp().getRequest().user = res;
+                }),
+                map(() => true),
+                catchError((err) => {
+                    this.logger.error(err);
+                    return of(false);
+                }),
+            );
     }
 }
